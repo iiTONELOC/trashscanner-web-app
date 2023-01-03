96b57f6a4354f63ceb1273d2334fc2b73c59008b
iiTONELOC <anthonytropeano@me.com>
2023-01-03 10:27:28 -0500

    Created a basic home/landing page to match the mock-up.
    The page is responsive and looks good on mobile. But has not implemented
    any functionality yet. The Sign up and login buttons are just
    placeholders for now. Their functionality will be implemented in a
    separate task.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,33 @@
+import './Home.css';
+import logoImage from '../../assets/images/logo.png';
+
 
 export default function Home(): JSX.Element {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <section className='Home-container Text-shadow'>
+            <header className='Home-header'>
+                <h1>WELCOME TO </h1>
+                <h1>TRASH<span>SCAN</span></h1>
+            </header>
+
+            <div className='Home-main'>
+                <strong>Lorem ipsum dolor</strong>
+                <p>
+                    {/* TO DO: Replace placeholder text */}
+                    sit amet consectetur adipisicing elit. Ducimus veniam, quae
+                    delectus incidunt voluptatem eligendi, quisquam eius
+                    similique quod in quo libero ullam iste placeat! Sapiente
+                    dolorum nemo nisi maxime?
+                </p>
+                <div className='Action-button-container'>
+                    <button className='Action-button Text-shadow'>SIGN UP</button>
+                    <button className='Button Text-shadow'>LOGIN</button>
+                </div>
+            </div>
+
+            <div className='Home-hero'>
+                <img alt='TrashScan Logo' src={logoImage} />
+            </div>
+        </section>
     );
 };
